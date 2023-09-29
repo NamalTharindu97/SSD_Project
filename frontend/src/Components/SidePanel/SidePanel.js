07bc62848ebcd6063e31023695d7ffea1ed558e7
namal tharindu <NamalA701997@gmail.com>
2023-09-29 13:49:24 +0530

    side bar setup

--- frontend/src/Components/SidePanel/SidePanel.js
+++ frontend/src/Components/SidePanel/SidePanel.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./SidePanel.css";
 
 const SidePanel = () => {
-	return <div className="SidePanel-Container">side panel</div>;
+	return (
+		<div className="SidePanel-Container">
+			<div className="SideBar">
+				<ul>
+					<li>Item 1</li>
+					<li>Item 2</li>
+					<li>Item 3</li>
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default SidePanel;
