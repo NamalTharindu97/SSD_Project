3660f6d3c43cbf69dabebdad53fdf746f05c0cf5
namal tharindu <NamalA701997@gmail.com>
2023-09-29 18:37:42 +0530

    employee add

--- frontend/src/Components/Form/Form.js
+++ frontend/src/Components/Form/Form.js
@@ -1,38 +1,107 @@
-import React from 'react';
-import './Form.css';
+import React, { useState } from "react";
+import "./Form.css";
 
-const Form = () => {
-  return (
-    <div className="Form-Container">
-      <div className="Employee-Form">
-        <form>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" />
+const Form = ({ addEmployee }) => {
+	const [formData, setFormData] = useState({
+		name: "",
+		age: 0,
+		phone: "",
+		nic: "",
+		email: "",
+		password: "",
+	});
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-          <label htmlFor="age">Age:</label>
-          <input type="number" id="age" name="age" />
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-          <label htmlFor="phone">Phone:</label>
-          <input type="tel" id="phone" name="phone" />
+		try {
+			await addEmployee(formData);
 
-          <label htmlFor="nic">NIC:</label>
-          <input type="text" id="nic" name="nic" />
+			setFormData({
+				name: "",
+				age: 0,
+				phone: "",
+				nic: "",
+				email: "",
+				password: "",
+			});
+		} catch (error) {
+			console.error("Error adding employee:", error);
+		}
+	};
+	return (
+		<div className="Form-Container">
+			<div className="Employee-Form">
+				<form onSubmit={handleSubmit}>
+					<label htmlFor="name">Name:</label>
+					<input
+						type="text"
+						id="name"
+						name="name"
+						value={formData.name}
+						onChange={handleChange}
+					/>
 
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" />
+					<label htmlFor="age">Age:</label>
+					<input
+						type="number"
+						id="age"
+						name="age"
+						value={formData.age}
+						onChange={handleChange}
+					/>
 
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
+					<label htmlFor="phone">Phone:</label>
+					<input
+						type="tel"
+						id="phone"
+						name="phone"
+						value={formData.phone}
+						onChange={handleChange}
+					/>
 
-          <div className="button-container">
-            <button type="submit" className="submit_btn">
-              Add Employee
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+					<label htmlFor="nic">NIC:</label>
+					<input
+						type="text"
+						id="nic"
+						name="nic"
+						value={formData.nic}
+						onChange={handleChange}
+					/>
+
+					<label htmlFor="email">Email:</label>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						value={formData.email}
+						onChange={handleChange}
+					/>
+
+					<label htmlFor="password">Password:</label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						value={formData.password}
+						onChange={handleChange}
+					/>
+
+					<div className="button-container">
+						<button type="submit" className="submit_btn">
+							Add Employee
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Form;
--- frontend/src/Pages/EmployeADD.js
+++ frontend/src/Pages/EmployeADD.js
@@ -2,14 +2,28 @@ import React from "react";
 import SidePanel from "../Components/SidePanel/SidePanel";
 import NavBar from "../Components/NavBar/NavBar";
 import Form from "../Components/Form/Form";
+import axios from "axios";
 
 const EmployeADD = () => {
+	const addEmployee = async (data) => {
+		try {
+			const response = await axios.post(
+				"http://localhost:5000/api/v1/employee/",
+				data
+			);
+			if (response) {
+				console.log(response.data);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 	return (
 		<div className="Dashboard-Container">
 			<SidePanel />
 			<div className="Home-Container">
 				<NavBar />
-				<Form />
+				<Form addEmployee={addEmployee} />
 			</div>
 		</div>
 	);
