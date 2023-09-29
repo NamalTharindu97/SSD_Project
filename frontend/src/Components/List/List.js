64424b4d0357aaf3ebee498846f54f3945140373
namal tharindu <NamalA701997@gmail.com>
2023-09-29 17:35:27 +0530

    delete function

--- frontend/src/Components/List/List.js
+++ frontend/src/Components/List/List.js
@@ -1,7 +1,8 @@
+// List.js
 import React from "react";
 import "./List.css";
 
-const List = () => {
+const List = ({ data, deleteEmployee }) => {
 	return (
 		<div className="List-Container">
 			<table>
@@ -16,17 +17,23 @@ const List = () => {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td>
-							<button>Update</button>
-							<button>Delete</button>
-						</td>
-					</tr>
+					{data.map((employee) => (
+						<tr key={employee.id}>
+							<td>{employee.name}</td>
+							<td>{employee.age}</td>
+							<td>{employee.phone}</td>
+							<td>{employee.nic}</td>
+							<td>{employee.email}</td>
+							<td>
+								<button>Update</button>
+								<button
+									onClick={() => deleteEmployee(employee._id)}
+								>
+									Delete
+								</button>
+							</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
 		</div>
--- frontend/src/Pages/DashboardPage.js
+++ frontend/src/Pages/DashboardPage.js
@@ -1,16 +1,47 @@
 // DashboardPage.js
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SidePanel from "../Components/SidePanel/SidePanel";
 import NavBar from "../Components/NavBar/NavBar";
 import List from "../Components/List/List";
+import axios from "axios";
 
 const DashboardPage = () => {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(
+					"http://localhost:5000/api/v1/employee/"
+				);
+				const result = await response.json();
+				setData(result);
+			} catch (error) {
+				console.error("error fetching data :", error);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	const deleteEmployee = async (routeID) => {
+		try {
+			const response = await axios.delete(
+				`http://localhost:5000/api/v1/employee/${routeID}`
+			);
+			if (response) {
+				window.location.reload();
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 	return (
 		<div className="Dashboard-Container">
 			<SidePanel />
 			<div className="Home-Container">
 				<NavBar />
-				<List />
+				<List data={data} deleteEmployee={deleteEmployee} />
 			</div>
 		</div>
 	);
