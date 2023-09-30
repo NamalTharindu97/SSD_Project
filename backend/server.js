cb393e3b6cc7cfe9b7ec4fa68d4ac56fc8d429a7
namal tharindu <NamalA701997@gmail.com>
2023-09-30 16:25:34 +0530

    login backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dbConnection = require("../backend/config/dbConnection");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -12,6 +14,15 @@ const PORT = process.env.PORT || 5001;
 //Security Misconfigurations
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+	session({
+		secret: "key-fsdfsrfeg165654gregsdgreg1e1g6re546464g5r6",
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+
 app.use("/api/v1/employee", require("../backend/routes/EmpRoutes"));
 
 app.listen(PORT, () => {
