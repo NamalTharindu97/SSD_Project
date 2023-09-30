4633570c710c262a6ec0d963e2494edda48fbf15
namal tharindu <NamalA701997@gmail.com>
2023-09-30 21:32:34 +0530

    privent mongodb injection and security misconfigurations

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 //mongodb injection
 const dbConnection = async () => {
 	try {
-		const con = await mongoose.connect(
-			"mongodb+srv://namal:namal@merncrudyt01.xrcuuxl.mongodb.net/?retryWrites=true&w=majority"
-		);
+		//use enviroment variables for privent mongodb injection
+		//use embedding credentials for privent mongdb injection
+
+		const userName = process.env.DB_NAME;
+		const password = process.env.DB_PASSWORD;
+
+		const connectionString = `mongodb+srv://${userName}:${password}@merncrudyt01.xrcuuxl.mongodb.net/?retryWrites=true&w=majority`;
+
+		const con = await mongoose.connect(connectionString);
 		console.log("db connected success", con.connection.name);
 	} catch (error) {
 		console.log(error);
