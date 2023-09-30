2f4962f003ea6e18f7402a4876a3628edb4a0a79
namal tharindu <NamalA701997@gmail.com>
2023-09-30 22:46:46 +0530

    privent nodejs dos attack

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,23 @@ const cors = require("cors");
 const dbConnection = require("../backend/config/dbConnection");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
+const slowDown = require("express-slow-down");
 const dotenv = require("dotenv").config();
 
 const app = express();
+
+//use express-slow-down library for prevent nodejs dos attacks
+const speedLimiter = slowDown({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	delayAfter: 50, // allow 50 requests, then start slowing down
+	delayMs: 500, // delay each request by 500ms after exceeding limit
+});
+
 dbConnection();
 const PORT = process.env.PORT || 5001;
 
-//Node.js DOS
-
-//Security Misconfigurations
-//configure cors properly for privent security misconfigurations
+// Security Misconfigurations
+// Configure cors properly for preventing security misconfigurations
 const allowedOrigins = ["http://localhost:3000"];
 app.use(
 	cors({
@@ -20,7 +27,8 @@ app.use(
 		credentials: true,
 	})
 );
-//limit the JSON payload size for privent security misconfigurations
+
+// Limit the JSON payload size for preventing security misconfigurations
 app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 app.use(
@@ -30,6 +38,9 @@ app.use(
 		saveUninitialized: true,
 	})
 );
+
+// Apply the speed limiter middleware to all routes
+app.use(speedLimiter);
 
 app.use("/api/v1/employee", require("../backend/routes/EmpRoutes"));
 
